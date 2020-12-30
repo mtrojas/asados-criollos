0d25e671ae8bce943faa3de1248bf62582942e92
MT <mtrojas@protonmail.com>
2020-12-30 20:41:54 -0300

    feat: create layout and footer, add to pages and gatsby browser and ssr files

--- gatsby/src/pages/parrilleros.js
+++ gatsby/src/pages/parrilleros.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Nav from '../components/Nav';
 
 export default function ParrillerosPage() {
   return (
-    <div>
-      <Nav />
+    <>
       <p>Hey! I'm the Parrilleros page</p>
-    </div>
+    </>
   );
 }
--- gatsby/src/pages/quiero.js
+++ gatsby/src/pages/quiero.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Nav from '../components/Nav';
 
 export default function QuieroPage() {
   return (
-    <div>
-      <Nav />
+    <>
       <p>Hey! I'm the quiero mi asado page</p>
-    </div>
+    </>
   );
 }
