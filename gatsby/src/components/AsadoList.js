e445ad43d51960790b3168ced3c328c8083d0696
MT <mtrojas@protonmail.com>
2021-01-02 16:50:06 -0300

    style: add styles to the asadolist component

--- gatsby/src/components/AsadoList.js
+++ gatsby/src/components/AsadoList.js
@@ -1,27 +1,50 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Img from 'gatsby-image';
+import styled from 'styled-components';
+
+const AsadoGridStyles = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 4rem;
+  grid-auto-rows: auto auto 500px;
+`;
+
+const AsadoStyles = styled.div`
+  display: grid;
+  /* Take your row sizing not from the AsadoStyles div, but from the AsadoGridStyles  */
+  @supports not (grid-template-rows: subgrid) {
+    --rows: auto auto 1fr;
+  }
+  grid-template-rows: var(--rows, subgrid);
+  grid-row: span 3;
+  grid-gap: 1rem;
+  h2,
+  p {
+    margin: 0;
+  }
+`;
 
 function SingleAsado({ asado }) {
   return (
-    <div>
+    <AsadoStyles>
       <Link to={`/asado/${asado.slug.current}`}>
         <h2>
           <span className="mark">{asado.name}</span>
         </h2>
-        <p>{asado.toppings.map((topping) => topping.name).join(' | ')}</p>
-        <Img fluid={asado.image.asset.fluid} alt={asado.name} />
       </Link>
-    </div>
+      <p>{asado.toppings.map((topping) => topping.name).join(' | ')}</p>
+      <Img fluid={asado.image.asset.fluid} alt={asado.name} />
+    </AsadoStyles>
   );
 }
 
 export default function AsadoList({ asados }) {
   return (
-    <div>
+    <AsadoGridStyles>
       {asados.map((asado) => (
         <SingleAsado key={asado.id} asado={asado} />
       ))}
-    </div>
+    </AsadoGridStyles>
   );
 }
