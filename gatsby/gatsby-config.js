ed75b8665c6527b9bcd37695a82c0f26987443f7
MT <mtrojas@protonmail.com>
2021-01-02 12:49:51 -0300

    feat: add sanity and styled components plugins to gatsby config

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,1 +1,23 @@
-// This file is empty, but some people were reporting that it would not start unless they had an empty file. So here it is! You can delete the comment. Or replace it with your favourite shania twain lyrics.
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
+export default {
+  siteMetadata: {
+    title: `Asados Criollos`,
+    siteUrl: `https://www.asadoscriollos.cl`,
+    description: `Los mejores asados en tu casa`,
+  },
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-sanity`,
+      options: {
+        projectId: `nc5ctpgg`,
+        dataset: `production`,
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
+};
