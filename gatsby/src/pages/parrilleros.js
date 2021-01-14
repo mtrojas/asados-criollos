8d5309cdabfdd5d38562e7aa0eb3b198c1fb6e65
MT <mtrojas@protonmail.com>
2021-01-13 21:00:34 -0300

    feat: display parrilleros and style them

--- gatsby/src/pages/parrilleros.js
+++ gatsby/src/pages/parrilleros.js
@@ -1,9 +1,81 @@
 import React from 'react';
+import { graphql, Link } from 'gatsby';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
 
-export default function ParrillerosPage() {
+const ParrillerosGrid = styled.div`
+  display: grid;
+  grid-gap: 2rem;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+`;
+
+const ParrilleroStyle = styled.div`
+  a {
+    text-decoration: none;
+  }
+  .gatsby-image-wrapper {
+    height: 400px;
+  }
+  h2 {
+    transform: rotate(-2deg);
+    text-align: center;
+    font-size: 4rem;
+    margin-bottom: -2rem;
+    position: relative;
+    z-index: 2;
+  }
+  .description {
+    background: var(--yellow);
+    padding: 1rem;
+    margin: 2rem;
+    margin-top: -6rem;
+    z-index: relative;
+    position: relative;
+    transform: rotate(1deg);
+    text-align: center;
+  }
+`;
+
+export default function ParrillerosPage({ data }) {
+  const parrilleros = data.parrilleros.nodes;
   return (
     <>
-      <p>Hey! I'm the Parrilleros page</p>
+      <ParrillerosGrid>
+        {parrilleros.map((parrillero) => (
+          <ParrilleroStyle>
+            <Link to={`/parrillero/${parrillero.slug.current}`}>
+              <h2>
+                <span className="mark">{parrillero.name}</span>
+              </h2>
+            </Link>
+            <Img fluid={parrillero.image.asset.fluid} />
+            <p className="description">{parrillero.description}</p>
+          </ParrilleroStyle>
+        ))}
+      </ParrillerosGrid>
     </>
   );
 }
+
+export const query = graphql`
+  query {
+    parrilleros: allSanityPerson {
+      totalCount
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        description
+        image {
+          asset {
+            fluid(maxWidth: 410) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
